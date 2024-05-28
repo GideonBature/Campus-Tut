4505609701acdd9073561e01d5126f3c7f47c263
Toyin Obafemi <byhisgraceabba@gmail.com>
2024-05-28 16:09:33 +0100

    adding authentication routes

---
+++ routes/authRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const authController = require('../controllers/authController');
+
+const router = express.Router();
+
+router.post('/login', authController.login);
+router.post('/register', authController.register);
+router.post('/logout', authController.logout);
+
+module.exports = router;
