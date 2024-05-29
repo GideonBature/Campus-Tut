47c63bc42c0c0d0cffabe2a68ce48ce960bfd2e6
Haffie023 <joyce.muchoki@gmail.com>
2024-05-29 08:26:09 +0300

    controllers

---
+++ src/controllers/userController.js
@@ -0,0 +1,54 @@
+const { User } = require("../models/index");
+
+exports.getAllUsers = async (req, res) => {
+    const { role, department } = req.query;
+    const query = { };
+
+    if (role) query.role = role;
+    if (department) query.department = department;
+
+    try {
+        const users = await User.find(query);
+        res.json(users);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+exports.getUserById = async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.json(user);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+exports.updateUser = async (req, res) => {
+    try {
+
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.json(user);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+exports.deleteUser = async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.json({ message: "User deleted successfully" });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
